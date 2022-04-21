b1b5ebe077fb326b359a7a628bcd5302a2ed33fb
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-04-21 12:27:42 +0200

    adding-onPressResize

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -28,7 +28,20 @@ sap.ui.define([
 		// 		this.byId("btnLegend").setTooltip("Hide legend");
 		// 	}
 		// },
-
+     
+      onPressResize: function() {
+			if (this.byId("btnResize").getTooltip() == "Minimize") {
+				if (sap.ui.Device.system.phone) {
+					this.byId("vbi").minimize(132, 56, 1320, 560);// Height: 3,5 rem; Width: 8,25 rem
+				} else {
+					this.byId("vbi").minimize(168, 72, 1680, 720);// Height: 4,5 rem; Width: 10,5 rem
+				}
+				this.byId("btnResize").setTooltip("Maximize");
+			} else {
+				this.byId("vbi").maximize();
+				this.byId("btnResize").setTooltip("Minimize");
+			}
+		},
 
       
 
